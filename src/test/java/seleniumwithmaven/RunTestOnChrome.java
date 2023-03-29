c50e904d246126c8680c3f6f4b5f091e148a6718
Kartik Patel <kartik.patel@vertexinc.com>
2023-03-29 15:46:02 +0530

    Initial commit

---
+++ src/test/java/seleniumwithmaven/RunTestOnChrome.java
@@ -0,0 +1,31 @@
+package seleniumwithmaven;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class RunTestOnChrome {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void beforeMethod() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void test() {
+        driver.get("https://www.google.com");
+        driver.findElement(By.xpath("//input[@name='q']")).sendKeys("Kartik Chetan Patel");
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        driver.close();
+    }
+}
